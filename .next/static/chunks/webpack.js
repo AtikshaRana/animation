57c07b8fecd4cc8af7a50fb76391c8da956feced
Atiksha <atiksha@webenertia.com>
2023-10-26 12:54:34 +0530

    Add changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8c9efdf90dd76fe4"; }
+/******/ 		__webpack_require__.h = function() { return "9c1d1ca4c7bc3292"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
